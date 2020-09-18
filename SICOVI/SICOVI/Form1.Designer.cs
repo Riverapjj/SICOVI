de276ae4de76f9174be574b8c81bf921b00b9ba3
Josué Rivera <keko.rai.jr@gmai.com>
2020-09-17 23:57:10 -0600

    Commit desde merging

--- SICOVI/SICOVI/Form1.Designer.cs
+++ SICOVI/SICOVI/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 317);
+            this.ClientSize = new System.Drawing.Size(775, 319);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
